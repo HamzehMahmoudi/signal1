dcfa122107db5df341ba332683f14988ed9ab8dd
crooked2 <46393208+crooked2@users.noreply.github.com>
2020-10-09 00:26:08 +0330

    Initial commit

---
+++ signal.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import cv2
+import numpy as np
+img = cv2.imread('cameraman.png',0) # tabdil ax be array
+#plt.hist(img.ravel(),256,[0,256])  
+#plt.show()
+img2= cv2.equalizeHist(img)     # hamgen kardane array
+cv2.imwrite('cameraman2.png',img2)  #tabdil array hamgen shode be tasvir va zakhire ba esme cameraman2.png
+img3 = np.hstack((img,img2))   #kenar ham gozashtan array(baraye moghayese sade tar)
+cv2.imwrite('IOComparison.png',img3) # zakhire dar file iocomparison
