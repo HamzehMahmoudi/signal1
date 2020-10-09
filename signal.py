846183b56a57c0dead4db1ffb32dbb46972730fc
crooked2 <46393208+crooked2@users.noreply.github.com>
2020-10-09 13:09:31 +0330

    another way to remove noise added

--- signal.py
+++ signal.py
@@ -8,3 +8,6 @@ img2= cv2.equalizeHist(img)     # hamgen kardane array
 cv2.imwrite('cameraman2.png',img2)  #tabdil array hamgen shode be tasvir va zakhire ba esme cameraman2.png
 img3 = np.hstack((img,img2))   #kenar ham gozashtan array(baraye moghayese sade tar)
 cv2.imwrite('IOComparison.png',img3) # zakhire dar file iocomparison
+clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+clh= clahe.apply(img)
+cv2.imwrite('cameraman3.jpg',clh)
